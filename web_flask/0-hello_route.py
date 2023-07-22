031fef1b361efcf47bb258c169ed2c1cb26b4fdf
Dennis Muvaa <dmuvaa70@gmail.com>
2023-07-22 19:21:54 +0300

    v2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,14 @@ from flask import Flask
 
 
 app = Flask(__name__)
+
+
 def hello():
+    """
+    return hello
+    """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
