a11b28a8ceada6bfbe79c10ab9c66f079c747cb5
Dennis Muvaa <dmuvaa70@gmail.com>
2023-07-24 00:29:44 +0300

    Extended the Flask web application to include a new route

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,7 +45,8 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', n=n, parity='even' if n % 2 == 0 else 'odd')
+    return render_template('6-number_odd_or_even.html',
+                           n=n, parity='even' if n % 2 == 0 else 'odd')
 
 
 if __name__ == "__main__":
